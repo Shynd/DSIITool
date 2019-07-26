cb080eb557e35d0b02a80f6b16bafdafb11e27fb
Shynd <shynd@protonmail.com>
2019-07-26 12:14:52 +0200

    Add project files.

---
+++ DSIITool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSIITool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
